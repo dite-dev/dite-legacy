2d77dca6ed117f2502dfc0e7437c76445026f426
Zack <lorenx@163.com>
2022-10-21 14:01:12 +0800

    test: add tree-kill

---
+++ packages/dite/test/shared/lib/tree-kill.spec.ts
@@ -0,0 +1,17 @@
+import cp from 'child_process';
+import { describe, expect, it } from 'vitest';
+import { treeKillSync } from '../../../src/shared/lib/tree-kill';
+
+describe('shared/lib/tree-kill.spec.ts', () => {
+  let proc: cp.ChildProcess;
+
+  describe('treeKillSync', () => {
+    // it('should be success', () => {
+    //
+    // })
+    it('should use tree-kill to kill process', () => {
+      expect(treeKillSync).toBeDefined();
+      // expect(treeKillSync).toHaveBeenNthCalledWith(Number(proc?.pid), 'SIGTERM')
+    });
+  });
+});
