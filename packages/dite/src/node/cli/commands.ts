85af3e5d9bc2e579dfddedc3ed1f7add74219590
Zack <lorenx@163.com>
2022-10-21 13:16:57 +0800

    feat: update server

--- packages/dite/src/node/cli/commands.ts
+++ packages/dite/src/node/cli/commands.ts
@@ -1,62 +1,31 @@
 import { readConfig, ServerMode } from '@dite/core';
-import { fork } from 'child_process';
-import { spawn } from 'cross-spawn';
 import exitHook from 'exit-hook';
 import ora from 'ora';
-import { getMemoryUsage } from '../../shared/lib/print-memory-usage';
-import { treeKillSync as killProcessSync } from '../../shared/lib/tree-kill';
 import * as compiler from '../compiler';
+import { createServer } from '../server';
 
 export async function watch(diteRoot: string) {
   const config = await readConfig(diteRoot);
 
-  let childProcessRef: any;
+  const server = await createServer(config);
 
   const spinner = ora('dite');
-
-  const createServer = () => {
-    const childRef = fork(config.serverBuildPath, {
-      env: process.env,
-      stdio: 'inherit',
-    });
-
-    childRef.on('message', (msg) => {
-      if (msg === 'dite:ready') {
-        spinner.stop();
-        console.info(getMemoryUsage());
-        console.log('Server is ready.');
-      }
-    });
-
-    childRef.on('error', () => {
-      spinner.stop();
-    });
-
-    return childRef;
-  };
 
   const closeWatcher = await compiler.watch(config, {
     mode: 'development',
     async onInitialBuild() {
-      // spinner.stop();
-      childProcessRef = createServer();
+      await server.listen(config.port);
     },
     onRebuildStart: () => {
       spinner.start();
     },
     async onRebuildFinish() {
-      // spinner.stop();
-      childProcessRef.removeAllListeners('exit');
-      childProcessRef.on('exit', () => {
-        childProcessRef = createServer();
-        childProcessRef.on('exit', () => (childProcessRef = undefined));
-      });
-      childProcessRef.stdin && childProcessRef.stdin.pause();
-      killProcessSync(childProcessRef.pid);
+      spinner.stop();
+      await server.restart();
     },
   });
   return () => {
-    childProcessRef?.pid && killProcessSync(childProcessRef.pid);
+    server.close();
     closeWatcher();
   };
 }
@@ -67,16 +36,9 @@ export async function build(diteRoot: string) {
 }
 
 export async function start(diteRoot: string, opts: { port?: number }) {
-  const config = await readConfig(diteRoot);
-  const childProcessRef = spawn('node', [config.serverBuildPath], {
-    env: {
-      ...process.env,
-      PORT: String(opts.port),
-    },
-    stdio: 'inherit',
-    shell: true,
-  });
+  const server = await createServer(diteRoot);
+  await server.listen(opts.port);
   exitHook(() => {
-    childProcessRef?.pid && killProcessSync(childProcessRef.pid);
+    server.close();
   });
 }
