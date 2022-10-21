7e07d8510baae6be47b752a62b428d5a1d673b27
Zack <lorenx@163.com>
2022-10-21 21:44:50 +0800

    build: update cac

--- packages/dite/tsup.config.ts
+++ packages/dite/tsup.config.ts
@@ -1,5 +1,6 @@
 import type { Format } from 'tsup';
 import { defineConfig } from 'tsup';
+import pkg from './package.json';
 
 const banner = ({ format }: { format: Format }) => ({
   js:
@@ -27,6 +28,7 @@ export default defineConfig([
     clean: true,
     shims: true,
     format: ['cjs', 'esm'],
+    external: [...Object.keys(pkg.dependencies)],
     banner,
   },
   {
